12384e8d1164f88a1370fe4054c9a27cfdbbde66
Samiul2024 <mdsamiullahossen@gmail.com>
2026-06-16 17:41:58 +0600

    meta tag...

---
+++ client/src/data/seo.js
@@ -0,0 +1,20 @@
+export const SEO = {
+  title:
+    "Kazi Office Boyra | Marriage Registration Services in Khulna",
+
+  description:
+    "Government approved marriage registration services in Boyra, Khulna. Muslim marriage registration, Hindu marriage registration, marriage certificate assistance and legal documentation.",
+
+  keywords: [
+    "Kazi Office Boyra",
+    "Kazi Office Khulna",
+    "Marriage Registration Khulna",
+    "Muslim Marriage Registration",
+    "Hindu Marriage Registration",
+    "Nikah Registration",
+    "Marriage Certificate Khulna",
+    "Marriage Registrar Khulna",
+    "Boyra Marriage Registration",
+    "Khulna Kazi Office"
+  ]
+};
